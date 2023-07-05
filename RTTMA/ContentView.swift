88153051d67965d21948d2586adf75c0823de18d
Quisette Chung <74784464+Quisette@users.noreply.github.com>
2023-07-05 10:12:48 +0800

    Initial Commit

---
+++ RTTMA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RTTMA
+//
+//  Created by Quisette Chung on 2023/7/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RTTMA/RTTMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RTTMAApp.swift
+//  RTTMA
+//
+//  Created by Quisette Chung on 2023/7/5.
+//
+
+import SwiftUI
+
+@main
+struct RTTMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
